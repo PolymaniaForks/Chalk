5196e8e7a9c69ee056bc490135225e9a8d117214
DaFuqs <dominik.m.schmidt@googlemail.com>
2025-07-10 11:34:19 +0200

    update to 1.21.7

--- src/main/java/de/dafuqs/chalk/common/items/GlowChalkItem.java
+++ src/main/java/de/dafuqs/chalk/common/items/GlowChalkItem.java
@@ -1,13 +1,14 @@
 package de.dafuqs.chalk.common.items;
 
-import de.dafuqs.chalk.common.ChalkRegistry;
-import net.minecraft.block.Block;
-import net.minecraft.util.DyeColor;
+import de.dafuqs.chalk.common.*;
+import net.minecraft.block.*;
+import net.minecraft.util.*;
 
 public class GlowChalkItem extends ChalkItem {
     public GlowChalkItem(Settings settings, DyeColor dyeColor) {
         super(settings, dyeColor);
     }
+    
     public Block getChalkMarkBlock() {
         return ChalkRegistry.chalkVariants.get(this.dyeColor).glowChalkBlock;
     }
